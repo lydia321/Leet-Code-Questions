985d26cc9d44c64df3c1f14f8fb6b2740f8594d5
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-22 23:20:33 +0300

    Time: 1880 ms (11.20%), Space: 27.7 MB (95.85%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,17 +1,30 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        arr=[0]*len(nums)
-        arr[0]=nums[0]
+        arr=[0]*len(nums) #array of 0's *length
+        arr[0]=nums[0]  #add first element to start with
         
-        for i in range(1,len(nums)):
-            if arr[i-1] > 0:
+        for i in range(1,len(nums)): 
+            if arr[i-1] > 0:  #look at the value of last index of arr
                 arr[i]=nums[i]+arr[i-1]
             else:
                 arr[i]=nums[i]
                 
-        return max(arr)      
-                
-            
+        return max(arr)   
+    
+    
+#                 '''
+#                  [5,4,-1,7,8]
+#                  [0,0,0,0,0]
+#                  [5,0,0,0,0]
+#                  [5,9{4+5},0,0,0]
+#                  [5,9,8{9+-1},0,0]
+#                  [5,9,8,15{8+7},0]
+#                  [5,9,8,15,23{15+8}]
+#                   max=23
+                 
+                 
+#                 '''
+        
                        
         
      
