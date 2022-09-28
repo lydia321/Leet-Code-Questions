91eca58bb1c53014bd018d431f4e49046522fe3e
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-28 10:19:50 +0300

    Time: 153 ms (17.42%), Space: 13.9 MB (86.00%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -7,13 +7,12 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         a,b='',''
         while l1:
-            a=str(l1.val) + a #so it can print in reverse order
+            a= str(l1.val) +a #to reversee the order
             l1=l1.next
         while l2:
-            b=str(l2.val)+b  #so it can print in reverse order
+            b=str(l2.val) +b  #to reverse the order
             l2=l2.next
         sum=int(a)+int(b)
-        #print(sum)
         output=ListNode()
         curr=output
         for i in reversed(str(sum)):
