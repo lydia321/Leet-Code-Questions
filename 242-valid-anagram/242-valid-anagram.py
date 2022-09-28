e4b92cf55b000965003179092c7bf82daaa2ed72
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-28 16:34:24 +0300

    Time: 102 ms (30.97%), Space: 15.1 MB (11.66%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,16 +1,8 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         s=sorted(s)
         t=sorted(t)
-        answer=bool(1) 
-    
         if s==t:
-            return answer
+            return True
         else:
-            answer=bool(0)
-            return answer
+            return False
