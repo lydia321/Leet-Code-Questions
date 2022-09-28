610bbe22d0952b31602045767920d0f991893815
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-28 16:24:21 +0300

    Time: 90 ms (16.17%), Space: 14 MB (40.21%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,19 +1,10 @@
-import numpy as np
-class Solution(object):
-    def relativeSortArray(self, arr1, arr2):
-        """
-        :type arr1: List[int]
-        :type arr2: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         output=[]
         for i in arr2:
             if i in arr1:
-                for j in range(arr1.count(i)):
+                a=arr1.count(i)
+                for j in range(a):
                     output.append(i)
                     arr1.remove(i)
-        return output + sorted(arr1)        
-                       
-                       
-                      
-        
+        return output + sorted(arr1)            
