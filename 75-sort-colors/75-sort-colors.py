49f2013c6b8af591f613becdca6de7d9554e6a79
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-29 09:05:26 +0300

    Time: 83 ms (5.60%), Space: 13.9 MB (15.71%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,12 +1,7 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if nums[j] > nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        return nums            
+        nums.sort()
         
