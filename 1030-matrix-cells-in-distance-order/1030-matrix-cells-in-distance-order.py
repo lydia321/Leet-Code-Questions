6507a50d31c7f3387fa27e506e6c55d02ec0be9a
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-09-29 08:28:41 +0300

    Time: 356 ms (33.19%), Space: 15.8 MB (89.69%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
@@ -1,20 +1,8 @@
-class Solution(object):
-    def allCellsDistOrder(self, rows, cols, rCenter, cCenter):
-        """
-        :type rows: int
-        :type cols: int
-        :type rCenter: int
-        :type cCenter: int
-        :rtype: List[List[int]]
-        """
-        
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
         output=[]
         for i in range(rows):
             for j in range(cols):
                 output.append([i,j])
-        return sorted(output, key = lambda p: abs(p[0] - rCenter) + abs(p[1] - cCenter))       
-                
-                
-                
-                
+        return sorted(output, key= lambda p: abs(p[0]-rCenter) + abs(p[1]- cCenter))        
         
