f615e39fd524d9af302a66d9bcd934d642d3da0d
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-10-02 20:02:14 +0300

    Time: 57 ms (41.16%), Space: 13.9 MB (72.19%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lookup = { '}' : '{' , ']' : '[' , ')' : '('}
+        stack = []
+        stack.append(s[0])
+        i = 1
+        
+        while i < len(s):
+            if s[i] in lookup.values():
+                stack.append(s[i])
+            else:
+                if stack and stack[-1] == lookup[s[i]]:
+                    stack.pop()
+                else:
+                    return False
+            i+=1    
+        return stack == []        
