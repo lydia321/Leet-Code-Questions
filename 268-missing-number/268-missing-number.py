30bd002690baa06c7c5edb033eedb5750a1614a2
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-10-04 16:31:28 +0300

    Time: 7127 ms (5.00%), Space: 15.7 MB (6.62%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        n=set(nums)
+        count=0
+        while count in nums:
+            count+=1
+        return count    
