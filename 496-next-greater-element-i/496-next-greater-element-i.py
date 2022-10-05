4218c022eccf2154fd233932b23bbfe768fbae48
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-10-05 14:18:43 +0300

    Time: 108 ms (38.31%), Space: 14.2 MB (56.41%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,17 +1,18 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
         lookup = defaultdict(int)
-        stack=[]
-        output=[-1] * len(nums1)
+        output = [-1]*len(nums1)
         
         for i,val in enumerate(nums1):
             lookup[val] = i
-         
+        
+        
         for i in range(len(nums2)):
             while stack and stack[-1] < nums2[i]:
                 temp = stack.pop()
                 output[lookup[temp]] = nums2[i]
-                    
-            if nums2[i] in lookup: stack.append(nums2[i])
                 
+            if nums2[i] in lookup: stack.append(nums2[i])    
+         
         return output
