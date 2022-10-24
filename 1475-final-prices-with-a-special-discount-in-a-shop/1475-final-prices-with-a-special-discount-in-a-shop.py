ad5e514642840886c3d5e14f5d429a3cb91aa525
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-10-24 16:12:19 +0300

    Time: 67 ms (79.01%), Space: 13.9 MB (62.00%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,13 +1,18 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        ans=[]
+        output = []
+        
         for i in range(len(prices)-1):
             for j in range(i+1,len(prices)):
-                if prices[i]>=prices[j]:
-                    ans.append(abs(prices[i]-prices[j]))
+                
+                if prices[i] >= prices[j]:
+                    output.append(prices[i]-prices[j])
                     break
+                    
             else:
-                ans.append(prices[i])
-           
-        ans.append(prices[-1])
-        return ans
+                output.append(prices[i])
+        output.append(prices[-1])
+        return output
+                    
+                    
+        
