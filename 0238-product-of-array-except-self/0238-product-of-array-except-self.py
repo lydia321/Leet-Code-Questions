5a62808fb644ca4e499061c0afd65a2f495616f9
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-11-09 16:54:06 +0300

    Time: 660 ms (22.21%), Space: 21.5 MB (41.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre,pos = [nums[0]], [nums[-1]]
-        
-        for i in range(1,len(nums)):
+        pre=[1]
+       
+        for i in range(len(nums)-1):
             pre.append(nums[i]*pre[-1])
+        print(pre)  
         
-        for i in range(len(nums)-2,-1,-1):
-            pos.append(nums[i]*pos[-1])
-        pos = pos[::-1]
-        print(pos)
-        print(pre)
-        output = [pos[1]]
-        for i in range(1,len(nums)-1):
-            output.append(pre[i-1]*pos[i+1])
-        output.append(pre[-2])
-        return output    
+        pos = 1
+        for i in range(len(nums)-1,-1,-1):
+            
+            pre[i] *= pos
+            pos *= nums[i]
+            
+        return pre
+#         pre,pos = [nums[0]], [nums[-1]]
+        
+#         for i in range(1,len(nums)):
+#             pre.append(nums[i]*pre[-1])
+        
+#         for i in range(len(nums)-2,-1,-1):
+#             pos.append(nums[i]*pos[-1])
+#         pos = pos[::-1]
+       
+#         output = [pos[1]]
+#         for i in range(1,len(nums)-1):
+#             output.append(pre[i-1]*pos[i+1])
+#         output.append(pre[-2])
+#         return output    
             
         
             
