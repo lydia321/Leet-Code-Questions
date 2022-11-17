52507c462c3fb6294d401d94611ac8d2dd7edc1c
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-11-17 10:41:36 +0300

    Time: 32 ms (94.98%), Space: 13.9 MB (63.18%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,14 +4,12 @@ class Solution:
          heapq.heapify(stones)
         
          while len(stones) > 1:
-                
-            # remove two stones
              a = heapq.heappop(stones)
              b = heapq.heappop(stones)
-             if a != b:
-                    
-                 diff = abs(a)-abs(b)
-                 heapq.heappush(stones, -diff)
+             
+             if a != b:    
+                  diff = abs(a)-abs(b)
+                  heapq.heappush(stones, -diff)
             
          if len(stones) == 0:
              return 0
