f5a8c1eb2f693a81a880c4d27862f4be38c16a0e
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-12-07 13:29:19 +0300

    Time: 985 ms (95.67%), Space: 24.8 MB (73.42%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -3,18 +3,17 @@ class Solution:
         ans = float("inf")
         total = sum(nums)
         l = len(nums)
-        idx = 0
-        prefix_sum = 0
+        idx = prefix_sum = 0
         
         for i in range(len(nums)):
             total -= nums[i]
             prefix_sum += nums[i]
             l -= 1
             a = prefix_sum//(i+1)
-            if l != 0:
-                b = total//l
-            else:
-                b = 0
+            
+            if l != 0: b = total//l
+            else: b = 0
+            
             res = abs(a-b)
             if res < ans:
                 ans = res
