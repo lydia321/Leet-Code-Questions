f3cc5e12a67773e0d0bae63d3226bfe9e06f007a
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-12-15 11:36:50 +0300

    Time: 1041 ms (85.60%), Space: 20.1 MB (48.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -5,11 +5,12 @@ class Solution:
         
         for r in range(len(fruits)):
             curr[fruits[r]] = 1 + curr.get(fruits[r],0)
+            
             while len(curr) > 2:
                 curr[fruits[l]] -= 1
                 
                 if curr[fruits[l]] == 0:
-                    del curr[fruits[l]]
+                    curr.pop(fruits[l])
                 l += 1
             res = max(res,sum(curr.values()))
         return res
