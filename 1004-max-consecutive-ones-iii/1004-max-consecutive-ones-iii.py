79bb100bbc1d2fd714e509f2c691567fc2a8e3b5
lydia321 <42097132+lydia321@users.noreply.github.com>
2022-12-15 17:07:30 +0300

    Time: 606 ms (94.66%), Space: 14.7 MB (61.94%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -5,6 +5,7 @@ class Solution:
         for r in range(len(nums)):
             if nums[r] == 0:
                 zeros += 1
+            
             while zeros > k:
                 if nums[l] == 0: zeros -= 1
                 l += 1
