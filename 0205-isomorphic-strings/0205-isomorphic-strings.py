4ff338385cf02afa0dbafb2ca9986eae31b4fc35
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-02 10:58:27 +0300

    Time: 41 ms (88.16%), Space: 14.2 MB (88.75%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         sMap = {}
+        tMap = {}
         for i in range(len(s)):
-            if s[i] not in sMap :
-                if t[i] in sMap.values():
-                    return False
+            if s[i] not in sMap and t[i] not in tMap :
                 sMap[s[i]] = t[i]
-            else:
+                tMap[t[i]] = s[i]
+            elif s[i] in sMap :
                 if sMap[s[i]] != t[i]:
+                    return False
+            elif t[i] in tMap:
+                if tMap[t[i]] != s[i]:
                     return False
         return True
     
