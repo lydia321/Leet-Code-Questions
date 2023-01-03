44876c842b1fab385424774f0ee96a7e8f313876
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-03 15:53:53 +0300

    Time: 107 ms (26.91%), Space: 13.9 MB (75.78%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        lookup = {"I": 1,"V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        
+        for i in range(len(s)):
+            if i + 1 < len(s) and lookup[s[i]] < lookup[s[i+1]]:
+                res -= lookup[s[i]]
+            else:
+                res += lookup[s[i]]
+        return res
