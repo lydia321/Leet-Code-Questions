ef1e3eb39b2f3092285aae24e746594b2d67f816
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-11 21:40:32 +0300

    Time: 245 ms (82.71%), Space: 14.7 MB (81.20%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        number_of_apperance = len(s)//4
+        # print(number_of_apperance)
+        lookup = {}
+        
+        for i in range(len(s)):
+            lookup[s[i]] = 1 + lookup.get(s[i],0)
+        if max(lookup.values()) == number_of_apperance:
+            return 0
+        print(lookup)    
+        res = len(s)
+        l = 0
+        for r in range(len(s)):
+            if s[r] in lookup: lookup[s[r]] -= 1
+            while max(lookup.values()) <= number_of_apperance:
+                res = min(res, r - l + 1)
+                if s[l] in lookup: lookup[s[l]] += 1
+                l += 1
+        return res
+        # print(lookup, number_of_apperance, len(s))
+#         for val,count in lookup.items():
+#             curr = max(count)
+#             if count > number_of_apperance:
+#                 print(count)
+#                 res += count - number_of_apperance 
+         
+#         return res
