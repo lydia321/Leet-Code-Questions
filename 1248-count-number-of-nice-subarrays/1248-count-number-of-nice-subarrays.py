b61c972e74c0906098275add87f8a0c436489d81
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-11 18:11:36 +0300

    Time: 897 ms (78.34%), Space: 21.7 MB (30.19%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,41 +1,15 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        remainders = []
-        rem = 0
-        res = 0
-        prefix = {0: 1}
+        stack ,Map = [] , {0:1}
+        curr = res = 0
+       
         for num in nums:
-            rem += num % 2
-            remainders.append(rem)
-        print(remainders)
+            curr += num % 2
+            stack.append(curr)
+        print(stack)
         
-        for remainder in remainders:
-            if remainder - k in prefix:
-                res += prefix[remainder - k]
-            prefix[remainder] = 1 + prefix.get(remainder, 0)
-            # if remainder not in prefix:
-            # 	prefix[remainder] =1
-            # else:
-            # 	prefix[remainder] += 1
+        for i in stack: 
+            Map[i] = 1 + Map.get(i, 0)
+            if i - k in Map:
+                res += Map[i - k]
         return res
-#         N = []
-#         for i in nums:
-#             if i%2 == 0:
-#                 N.append(0)
-#             else:
-#                 N.append(1)
-#         print(N)
-        
-#         l = 0
-#         res = 0
-#         curr = 0
-#         for r in range(len(N)): 
-#             curr += N[r]
-#             if curr == k:
-#                 res += 1
-#             while (curr - k) == 0:
-#                 curr -= N[l]
-#                 l += 1
-           
-#         return res
-            
