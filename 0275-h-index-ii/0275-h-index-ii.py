4cf79a67be4384e2bbc192ea842a26cae82e0301
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-13 09:47:56 +0300

    Time: 150 ms (80.82%), Space: 20.6 MB (38.36%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        l = 0
+        r = len(citations)-1
+        
+        if len(citations) == 0:
+            return 0
+        
+        while l < r:
+            m = (l+r)//2
+            if len(citations) - m <= citations[m]:
+                r = m 
+            else:
+                l = m + 1
+        
+        if citations[l] == 0:
+            return 0
+        return len(citations) - r
+
+    
