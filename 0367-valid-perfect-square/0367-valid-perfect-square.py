927607b0e46e7e95ad5db345f8a48c4a7359dca9
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-15 08:16:30 +0300

    Time: 28 ms (91.91%), Space: 13.8 MB (52.06%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -5,11 +5,11 @@ class Solution:
         
         while l <= r:
             m = (l+r)//2
-            if m*m > num:
-                r = m - 1
+            if m*m == num:
+                return True
             elif m*m < num:
                 l = m + 1
             else:
-                return True
+                r = m - 1
         return False
             
