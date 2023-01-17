cab1de90b8155482290c43c351a85357e5e19ccf
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-17 11:13:11 +0300

    Time: 48 ms (84.92%), Space: 13.8 MB (97.06%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -7,17 +7,3 @@ class Solution:
             if nums[i] == target:
                 res.append(i)
         return res
-#         l = 0
-#         r = len(nums)-1
-        
-#         while l <= r:
-#             m = (l+r)//2
-            
-#             if nums[m] == target:
-#                 res.append(m)
-                
-#             if nums[m] < target:
-#                 l = m + 1
-#             else:
-#                 r = m - 1
-        
