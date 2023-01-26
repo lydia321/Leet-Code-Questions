04c0acc52e03bb6e046bab92c2bf2b71890acb9a
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-01-26 11:20:12 +0300

    Time: 3645 ms (7.97%), Space: 33.4 MB (56.52%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -4,15 +4,16 @@ class Solution:
         heapq.heapify(heap)
         for i in range(min(k, len(nums1))):
             for j in range(min(k, len(nums2))):
+                val = (-(nums1[i]+nums2[j]),[nums1[i],nums2[j]])
                 if len(heap) < k:
-                    heapq.heappush(heap,(-(nums1[i]+nums2[j]),[nums1[i],nums2[j]]))
+                    heapq.heappush(heap,(val))
                 # print(heap)
                 else:
                     if(nums1[i]+nums2[j]) > -heap[0][0]:
                         break
                     else:
                         heapq.heappop(heap)
-                        heapq.heappush(heap,(-(nums1[i]+nums2[j]),[nums1[i],nums2[j]]))
+                        heapq.heappush(heap,(val))
         # print(heap)
         res = []
         for i in heap:
