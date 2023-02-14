682d35ae4f9dd61a5e4e9c006a7151510e7013bb
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-02-14 09:11:20 +0300

    Time: 815 ms (14.30%), Space: 13.9 MB (5.73%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        #Base case
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        #Recursion Case
+        return self.fib(n -1) + self.fib(n - 2)
+        
+            
+        
