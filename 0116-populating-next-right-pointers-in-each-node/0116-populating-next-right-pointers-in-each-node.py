b10b1746c889dfbb168239ca5af0ebf686d6e863
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-02-23 20:46:46 +0300

    Time: 73 ms (29.73%), Space: 15.4 MB (95.72%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -11,21 +11,41 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
-            return
-        queue = [root]
+                return None
+        curr = root
+        Next_curr = curr.left
         
-        while queue:
-            length = len(queue)
+        while curr and Next_curr:
             
-            for i in range(length):
-                curr = queue.pop(0)
-                
-                if i < length - 1:
-                    curr.next = queue[0]
-                else:
-                    curr.next = None
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
+            curr.left.next = curr.right
+            if curr.next:
+                curr.right.next = curr.next.left
+            curr = curr.next
+            if not curr:
+                curr = Next_curr
+                Next_curr = curr.left
         return root
+        
+            
+        
+        
+        #O(N) time complexity and O(N) Space complexity
+#         if not root:
+#             return
+#         queue = [root]
+        
+#         while queue:
+#             length = len(queue)
+            
+#             for i in range(length):
+#                 curr = queue.pop(0)
+                
+#                 if i < length - 1:
+#                     curr.next = queue[0]
+#                 else:
+#                     curr.next = None
+#                 if curr.left:
+#                     queue.append(curr.left)
+#                 if curr.right:
+#                     queue.append(curr.right)
+#         return root
