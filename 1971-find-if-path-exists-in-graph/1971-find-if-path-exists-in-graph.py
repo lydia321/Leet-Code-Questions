d2232827bb2e5541e69afb1dd261a3ec111322a4
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-02-27 15:57:05 +0300

    Time: 2501 ms (25.26%), Space: 306.5 MB (23.47%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,20 +1,41 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        #DFS
         graph = defaultdict(list)
         for i in edges:
             graph[i[0]].append(i[1])
             graph[i[1]].append(i[0])
-        # print(graph)
-        queue = [source]
-        visited = set({source})
+        print(graph)
+        visited = set()
         
-        while queue:
-            curr = queue.pop(0)
-            if curr == destination:
+        def dfs(root):
+            visited.add(root)
+            if root == destination:
                 return True
-            for i in graph[curr]:
-                if i not in visited:
-                    visited.add(i)               
-                    queue.append(i)
-        return False
+            for i in graph[root]:
+                if i not in visited and dfs(i):
+                    return True
+                
+            return False
+        return dfs(source)
+        
+        
+        #BFS 
+#         graph = defaultdict(list)
+#         for i in edges:
+#             graph[i[0]].append(i[1])
+#             graph[i[1]].append(i[0])
+#         # print(graph)
+#         queue = [source]
+#         visited = set({source})
+        
+#         while queue:
+#             curr = queue.pop(0)
+#             if curr == destination:
+#                 return True
+#             for i in graph[curr]:
+#                 if i not in visited:
+#                     visited.add(i)               
+#                     queue.append(i)
+#         return False
        
