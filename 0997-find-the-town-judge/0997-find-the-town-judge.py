be412f3804462c4b14d4e2a2f6555191973f9f9a
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-03 09:08:21 +0300

    Time: 729 ms (92.65%), Space: 18.8 MB (91.04%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:   
+        
+        graph = defaultdict(int)
+        
+        for i in trust:
+            graph[i[0]] -= 1
+            graph[i[1]] += 1
+        print(graph)
+        
+        for i in range(1,n+1):
+            if graph[i] == n - 1:
+                return i
+        return -1
+        
+        
+        
+        
+         
+        
+        
+        
