782a1f6da2dbaae0be84f8426f54406890efecfa
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-21 18:05:06 +0300

    Time: 107 ms (37.78%), Space: 17 MB (50.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,18 +2,19 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {}
         visited = set()
+        
         for i in range(numCourses):
-            graph[i] = []
+            if i not in graph:
+                graph[i] = []
         for i in prerequisites:
-            graph[i[1]].append(i[0])
+            graph[i[0]].append(i[1])
+        print(graph)
         
         def dfs(root):
-            if root in visited:
-                return False
-            
             if graph[root] == []:
                 return True
-            
+            if root in visited:
+                return False
             visited.add(root)
             for j in graph[root]:
                 if not dfs(j):
@@ -21,8 +22,10 @@ class Solution:
             visited.remove(root)
             graph[root] = []
             return True
+            
         
         for i in graph:
             if not dfs(i):
                 return False
         return True
+       
