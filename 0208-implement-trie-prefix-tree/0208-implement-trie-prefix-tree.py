ad5c12c75bb69f0b9c710f90682ba58c73d69ed3
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-21 09:56:23 +0300

    Time: 182 ms (48.17%), Space: 31.7 MB (47.98%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,42 +1,42 @@
 class TrieNode:
     def __init__(self):
-        self.children={}
-        self.end=False
+        self.children = {}
+        self.end = False
 class Trie:
-
     def __init__(self):
-        self.root=TrieNode()
-        
-
+        self.trie = TrieNode()
+               
     def insert(self, word: str) -> None:
-        curr=self.root
+        curr = self.trie
+        
         for i in word:
             if i not in curr.children:
-                curr.children[i]=TrieNode()
-            curr=curr.children[i]
-        curr.end=True
-
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        curr.end = True        
+        
     def search(self, word: str) -> bool:
-        curr=self.root
+        curr = self.trie
+        
         for i in word:
             if i not in curr.children:
                 return False
-            curr=curr.children[i]
+            curr = curr.children[i]
         return curr.end
-
+        
     def startsWith(self, prefix: str) -> bool:
-        curr=self.root
-        for i in prefix:
-            if i not in curr.children:
-                return False
-            curr=curr.children[i]
-        return True
+            curr = self.trie
+            
+            for i in prefix:
+                if i not in curr.children:
+                    return False
+                curr = curr.children[i]
+            return True
+        
+ 
 
-
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+# # Your Trie object will be instantiated and called as such:
+# # obj = Trie()
+# # obj.insert(word)
+# # param_2 = obj.search(word)
+# # param_3 = obj.startsWith(prefix)
