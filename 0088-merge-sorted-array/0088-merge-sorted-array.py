ab565b56075b5034305cad209fa1d97ebc802e04
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-28 19:28:24 +0300

    Time: 33 ms (90.93%), Space: 13.9 MB (74.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,37 +2,23 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-    
         """
-        # O(n) time comlexity     
-        #last index of nums1
-        last_idx=len(nums1)-1
-        #while both nums have value
+        idx = len(nums1) - 1
+        
         while m > 0 and n > 0:
-            if nums1[m-1]>nums2[n-1]:
-                nums1[last_idx]=nums1[m-1]
-                m-=1
-            else: #if they less than or equal
-                nums1[last_idx]=nums2[n-1]
-                n-=1
-            last_idx-=1
-        #if there are leftover elements in nums2
-        while n>0:
-            nums1[last_idx]=nums2[n-1]
-            last_idx-=1
-            n-=1
-            
-      # 2)  O(nlogn)  
-    
-        #del nums1[:m]
-        #nums1.extend(nums2)
-        #nums2.sort()
+            if nums1[m-1] > nums2[n-1]:
+                nums1[idx] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[idx] = nums2[n-1]
+                n -= 1
+            idx -= 1
+        #lleftover values for nums2
+        while n > 0:
+            nums1[idx] = nums2[n-1]
+            n -= 1
+            idx -= 1
         
-
-        
-            
-                
-                
-                
-            
-          
+        # del nums1[:n]
+        # nums1.extend(nums2)
+        # nums1.sort()
