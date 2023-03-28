3f633ebb296e4a80b6cbae9b6f2cb1cbda7d8400
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-28 23:50:38 +0300

    Time: 4940 ms (6.56%), Space: 17.6 MB (89.48%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,11 +5,14 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0 or not list:
+            return None
         
         def mergeList(l1,l2):
             res = ListNode(None)
             dummy = res
             
+            # when both l1 and l2 exits
             while l1 and l2:
                 if l1.val > l2.val:
                     dummy.next = l2
@@ -18,29 +21,25 @@ class Solution:
                     dummy.next = l1
                     l1 = l1.next
                 dummy = dummy.next
+                        
+            #leftover l1
             while l1:
                 dummy.next = l1
                 l1 = l1.next
                 dummy = dummy.next
+                
+            #leftover l2
             while l2:
                 dummy.next = l2
                 l2 = l2.next
                 dummy = dummy.next
+            
             return res.next
         
-        if len(lists) == 0 or not lists:
-            return None
-        
         queue = lists
-        
         while len(queue) > 1:
-            l1 = queue.pop(0)
-            l2 = queue.pop(0)
+            list1 = queue.pop()
+            list2 = queue.pop()
             
-            potential_res = mergeList(l1,l2)
-            queue.append(potential_res)
+            queue.append(mergeList(list1,list2))
         return queue[0]
-    
-                
-                
-        
