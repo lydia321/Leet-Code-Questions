ec9cee1f5d44970768d965b6fe4120e073b9fb69
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-28 14:30:41 +0300

    Time: 613 ms (20.04%), Space: 25.3 MB (43.67%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,37 +1,39 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edge: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        edges = defaultdict(int)  
-        #if graph only has two nodes
-        if n <= 2:
+        #if there are less than 2 nodes:
+        if n < 3:
             return list(range(n))
+        
+        graph = defaultdict(list)
+        edges = defaultdict(int)
         
         for i in edge:
             graph[i[0]].append(i[1])
             graph[i[1]].append(i[0])
             edges[i[0]] += 1
             edges[i[1]] += 1
+        # print(graph, edges)
         queue = []
-        # print(graph,edges)
         for i in edges:
             if edges[i] == 1:
                 queue.append(i)
-        current_count = n
-        while current_count > 2:
-            current_count -= len(queue)
-            temp_res = []
+        # print(queue)
+        remaining_count = n
+        
+        while remaining_count > 2:
+            possible_res = []
             while queue:
                 for _ in range(len(queue)):
-                    curr = queue.pop()
-                                        
-                    #Since curr has only one edge
+                    remaining_count -= 1
+                    curr = queue.pop(0)
+                    # since it only has one neighbour
                     only_nei = graph[curr].pop()
-                    #remove curr from its only nei also
                     graph[only_nei].remove(curr)
+                    edges[only_nei] -= 1
+                    edges[curr] -= 1
                     if len(graph[only_nei]) == 1:
-                        temp_res.append(only_nei)
-            queue = temp_res
-                        
-            
+                        possible_res.append(only_nei)
+            queue = possible_res
         return queue
-              
+                    
+                    
