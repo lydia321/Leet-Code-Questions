85e40153d5dd9c1afff71d6c57feedd48511f859
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-28 14:04:54 +0300

    Time: 550 ms (38.14%), Space: 26.4 MB (20.62%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edge: List[List[int]]) -> List[int]:
+        graph = defaultdict(set)
+        edges = defaultdict(int)  
+        #if graph only has two nodes
+        if n <= 2:
+            return list(range(n))
+        
+        for i in edge:
+            graph[i[0]].add(i[1])
+            graph[i[1]].add(i[0])
+            edges[i[0]] += 1
+            edges[i[1]] += 1
+        queue = []
+        # print(graph,edges)
+        for i in edges:
+            if edges[i] == 1:
+                queue.append(i)
+        current_count = n
+        while current_count > 2:
+            current_count -= len(queue)
+            temp_res = []
+            while queue:
+                for _ in range(len(queue)):
+                    curr = queue.pop()
+                                        
+                    #Since curr has only one edge
+                    only_nei = graph[curr].pop()
+                    #remove curr from its only nei also
+                    graph[only_nei].remove(curr)
+                    if len(graph[only_nei]) == 1:
+                        temp_res.append(only_nei)
+            queue = temp_res
+                        
+            
+        return queue
+              
