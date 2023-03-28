b49e31381737238766518eb5c2205a62724ea3e3
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-28 20:06:16 +0300

    Time: 45 ms (19.77%), Space: 13.9 MB (66.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,25 +5,70 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        res = ListNode(0)
-        dummy = res
+        dummy = ListNode(None)
+        res = dummy
+        
         while list1 and list2:
-            if list1.val < list2.val:
-                res.next = list1
-                list1 = list1.next
-            else:
-                res.next = list2
+            if list1.val > list2.val:
+                dummy.next = list2
                 list2 = list2.next
-            res = res.next
-        
+            else:
+                dummy.next = list1
+                list1 = list1.next
+            dummy = dummy.next
+        # if remaining values in list1
         while list1:
-            res.next = list1
+            dummy.next = list1
             list1 = list1.next
-            res = res.next
-        
+            dummy = dummy.next
+         # if remaining values in list2
         while list2:
-            res.next = list2
+            dummy.next = list2
             list2 = list2.next
-            res = res.next
-        return dummy.next
+            dummy = dummy.next
+        return res.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = ListNode(0)
+#         dummy = res
+#         while list1 and list2:
+#             if list1.val < list2.val:
+#                 res.next = list1
+#                 list1 = list1.next
+#             else:
+#                 res.next = list2
+#                 list2 = list2.next
+#             res = res.next
+        
+#         while list1:
+#             res.next = list1
+#             list1 = list1.next
+#             res = res.next
+        
+#         while list2:
+#             res.next = list2
+#             list2 = list2.next
+#             res = res.next
+#         return dummy.next
             
