1c88660b43622e2441b4d5e5ec2bee190cdd3ab6
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-29 10:27:34 +0300

    Time: 101 ms (82.33%), Space: 18.6 MB (65.00%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,34 +2,16 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict = {}
         for i in nums:
-            dict[i]  = 1 + dict.get(i,0)
+            dict[i] = 1 + dict.get(i,0)
         print(dict)
         
         res = []
         heapq.heapify(res)
         
-        for i,count in  dict.items():
+        for i in dict:
             heapq.heappush(res,(dict[i],i))
             if len(res) > k:
-                heapq.heappop(res)
+                heapq.heappop(res)   
         print(res)
-        return [i[1] for i in res]    
-            
-        
-        
-#         dict = {}
-#         for i in nums:
-#             dict[i] = 1 + dict.get(i,0)
-#         print(dict)
-        
-#         heap = []
-#         for i in dict:
-#             heapq.heappush(heap,(-dict[i],i))
-#         print(heap)
-        
-#         res = []
-#         for i in range(k):
-#             val = heapq.heappop(heap)
-#             res.append(val[1])
-#         return res
-            
+        return [i[1] for i in res]
+       
