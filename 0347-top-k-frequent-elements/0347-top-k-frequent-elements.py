f0f45dab021b781b260c6c214c7f220c5cba2fa3
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-29 09:54:35 +0300

    Time: 104 ms (71.47%), Space: 18.5 MB (87.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,34 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict = {}
         for i in nums:
-            dict[i] = 1 + dict.get(i,0)
+            dict[i]  = 1 + dict.get(i,0)
         print(dict)
         
-        heap = []
-        for i in dict:
-            heapq.heappush(heap,(-dict[i],i))
-        print(heap)
-        
         res = []
-        for i in range(k):
-            val = heapq.heappop(heap)
-            res.append(val[1])
-        return res
+        heapq.heapify(res)
+        
+        for i,count in  dict.items():
+            heapq.heappush(res,(dict[i],i))
+            if len(res) > k:
+                heapq.heappop(res)
+        print(res)
+        return [i[1] for i in res]    
+            
+        
+        
+#         dict = {}
+#         for i in nums:
+#             dict[i] = 1 + dict.get(i,0)
+#         print(dict)
+        
+#         heap = []
+#         for i in dict:
+#             heapq.heappush(heap,(-dict[i],i))
+#         print(heap)
+        
+#         res = []
+#         for i in range(k):
+#             val = heapq.heappop(heap)
+#             res.append(val[1])
+#         return res
             
