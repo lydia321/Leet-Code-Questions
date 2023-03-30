b9f3199f1f5803501dcdaa9bb19384590a32acb0
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-03-30 14:50:49 +0300

    Time: 755 ms (36.85%), Space: 25 MB (12.35%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -2,24 +2,28 @@ from sortedcontainers import SortedList
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         curr = SortedList()
+        l = 0
         res = []
-        idx = 0
-        for i in range(len(nums)):
-            curr.add(nums[i])
+        
+        for r in range(len(nums)):
+            curr.add(nums[r])
             
-            #if len becomes greater than k
-            if len(curr) > k:
-                curr.remove(nums[idx])
-                idx += 1
+            if len(curr) > k:  #goes out of the window
+                curr.remove(nums[l])
+                l += 1
+                
             
             if len(curr) == k:
+                # if its even
                 if len(curr)%2 == 0:
-                    m1 = len(curr)//2
+                    m1 = len(curr) // 2
                     m2 = m1 - 1
                     res.append((curr[m1] + curr[m2])/2)
+                #if its odd
                 else:
-                    middle = len(curr)//2
-                    res.append(curr[middle])
+                    m = len(curr) // 2
+                    res.append(curr[m])
+        
         return res
             
             
