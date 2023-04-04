257cf7f19a773ae3c9918434fef05e71beecdfdd
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-04 17:55:30 +0300

    Time: 28 ms (93.71%), Space: 14 MB (48.17%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -8,33 +8,33 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         # if not root: return True
         queue = [root]
-        queue1 = []
-        queue2 = []
+        left_queue = []
+        right_queue = []
         A = queue.pop()
         if not A.left and not A.right:
             return True
         if A.left: 
-            queue1.append(A.left)
+            left_queue.append(A.left)
         else: return False
         if A.right:
-            queue2.append(A.right)
+            right_queue.append(A.right)
         else: return False
        
         
-        while queue1 and queue2:
-            curr1 = queue1.pop()
-            curr2 = queue2.pop()
+        while left_queue and right_queue:
+            curr1 = left_queue.pop()
+            curr2 = right_queue.pop()
             # print(curr1,curr2)
             if not curr1 and not curr2: continue
             if not curr1 or not curr2: return False
             
             if curr1.val != curr2.val:
                 return False
-            queue1.append(curr1.right)
-            queue1.append(curr1.left)
-            queue2.append(curr2.left)
-            queue2.append(curr2.right)
-        if len(queue1) == len(queue2):
+            left_queue.append(curr1.right)
+            left_queue.append(curr1.left)
+            right_queue.append(curr2.left)
+            right_queue.append(curr2.right)
+        if len(left_queue) == len(right_queue):
             return True
         return False
    
