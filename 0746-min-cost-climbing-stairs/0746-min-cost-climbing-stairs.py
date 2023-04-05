2ff3234231eeb170f9ea1677d09ba4947685bdec
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-05 10:59:28 +0300

    Time: 62 ms (51.43%), Space: 14.1 MB (32.22%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,9 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #Bottom Up DP
+        #[25,15,20,0]
+        #[6,105,5,5,4,102,3,2,100,1,0]
+        #Every idx will choose optimally from the two optimal solutions infront of it
+        #Base case 
         dp = [0,cost[-1]]
         
         for i in range(len(cost) - 2, -1, -1):
             dp.append(min(dp[-1] + cost[i],dp[-2] + cost[i]))
-        # print(dp)
+        #Finally is it optimal to skip the first step or not
         return min(dp[-1],dp[-2])
         
