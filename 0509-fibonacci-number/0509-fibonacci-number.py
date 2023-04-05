a1f046a8a761b14aa646524dc22d3892626eb7cb
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-05 11:25:05 +0300

    Time: 38 ms (43.76%), Space: 13.9 MB (41.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -8,4 +8,14 @@ class Solution:
         for i in range(n-2):
             dp.append(dp[-1] + dp[-2])
         return dp[-1] + dp[-2]    
-    
+        
+#         #Top Down DP
+#         def dp(n,visited):
+#             if n in visited:
+#                 return visited[n]
+#             else:
+#                 visited[n] = dp(n-1,visited) + dp(n-2,visited)
+#             return visited[n]
+            
+#         return dp(n,{0:0,1:1})
+        
