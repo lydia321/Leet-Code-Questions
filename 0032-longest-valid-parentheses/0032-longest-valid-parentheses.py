b475efa66a738631215d3df41421b551a8ac2946
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-12 18:18:37 +0300

    Time: 47 ms (49.89%), Space: 14.6 MB (65.13%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,35 +1,78 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         '''
-        () (()), ((
+        ()
         (())
-        )()())
+        ()()
         
         )()())()   4
+        01234567
+        [5]
+        
         )))
-        (()
+        []
         
         '''
         if s == "":
             return 0
         res = 0
         stack = [-1]
-        dp = [0]*len(s)
         
         for i in range(len(s)):
             if s[i] == "(":
                 stack.append(i)
-                dp[0] = 0
             else:
-                j = stack.pop()
-                # if i popped the last valid index
+                stack.pop()
                 if not stack:
-                    stack.append(i) #add new valid index
+                    stack.append(i)
                 else:
                     res = max(res,i - stack[-1])
-                    dp[i] = (i - j + 1) + dp[j - 1]
-                    
-        return max(dp)
+        return res
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if s == "":
+#             return 0
+#         res = 0
+#         stack = [-1]
+#         # dp = [0]*len(s)
+        
+#         for i in range(len(s)):
+#             if s[i] == "(":
+#                 stack.append(i)
+#                 # dp[0] = 0
+#             else:
+#                 j = stack.pop()
+#                 # if i popped the last valid index
+#                 if not stack:
+#                     stack.append(i) #add new valid index
+#                 else:
+#                     res = max(res,i - stack[-1])
+#                     # dp[i] = (i - j + 1) + dp[j - 1]
+#         return res            
+#         # return max(dp)
         
                 
         
