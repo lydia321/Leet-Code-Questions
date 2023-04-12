2584b5a168de49f0c663fe9cfd98ebd194ddc224
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-12 11:44:20 +0300

    Time: 42 ms (77.56%), Space: 14.8 MB (19.45%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -10,21 +10,26 @@ class Solution:
         (()
         
         '''
+        if s == "":
+            return 0
         res = 0
         stack = [-1]
+        dp = [0]*len(s)
         
         for i in range(len(s)):
             if s[i] == "(":
                 stack.append(i)
+                dp[0] = 0
             else:
-                stack.pop()
+                j = stack.pop()
                 # if i popped the last valid index
                 if not stack:
                     stack.append(i) #add new valid index
                 else:
                     res = max(res,i - stack[-1])
+                    dp[i] = (i - j + 1) + dp[j - 1]
                     
-        return res
+        return max(dp)
         
                 
         
