96f387ef9c4d75314dec899a833a8443eef5179c
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-13 12:01:21 +0300

    Time: 41 ms (84.33%), Space: 14.3 MB (14.37%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,24 +1,20 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp = {}
-        
+        memo = {}
         def dfs(i,j):
-            if (i,j) in dp:
-                return dp[(i,j)]
+            if (i,j) in memo:
+                return memo[(i,j)]
             if i >= len(s) and j >= len(p):
                 return True
             if j >= len(p):
                 return False
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
             
-            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-            
-            if (j + 1) < len(p) and (p[j + 1] == '*'):
-                dp[(i,j)] =   (dfs(i,j+2) or (match and dfs(i + 1,j) ))
-                return dp[(i,j)]
-        
+            if (j + 1 < len(p)) and p[j+1] == '*':
+                memo[(i,j)] = (dfs(i,j+2)) or (match and dfs(i+1,j))
+                return memo[(i,j)]
             if match:
-                dp[(i,j)] = dfs(i + 1,j + 1)
-                return dp[(i,j)]
-            dp[(i,j)] = False
+                memo[(i,j)] = dfs(i + 1, j + 1)
+                return memo[(i,j)]
             return False
         return dfs(0,0)
