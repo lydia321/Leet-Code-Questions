3e8e54f10bfab9511f9b7add9592856a99b290b9
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-28 17:08:27 +0300

    Time: 2971 ms (5.01%), Space: 487.7 MB (5.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -10,10 +10,8 @@ class Solution:
                 return 0
             if i == len(coins) or target < 0:
                 return amount + 1
-            res = dp(i + 1,target)
-            if target >= coins[i]:
-                res = min(res,dp(i,target - coins[i]) + 1)
-            cache[(i,target)] = res
+
+            cache[(i,target)] = min(dp(i + 1,target),dp(i,target - coins[i]) + 1)
             return cache[(i,target)]
         res = dp(0,amount)
         return res if res != amount + 1 else -1
