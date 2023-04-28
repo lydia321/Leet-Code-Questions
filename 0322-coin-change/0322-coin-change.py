cbfdc9bd6e3e485e8a70fb6611758be491d4ad6e
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-04-28 17:07:17 +0300

    Time: 3083 ms (5.01%), Space: 488 MB (5.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        fewest_coin = 1
-        visited_coins = set(coins)
-        queue = deque(coins)
+        cache = {}
         
-        while queue:
-            for _ in range(len(queue)):
-                curr_coin = queue.popleft()
-                if amount == curr_coin:
-                    return fewest_coin
-                
-                for each in coins:
-                    possible_coin = curr_coin + each
-                    if possible_coin not in visited_coins and possible_coin <= amount:
-                        visited_coins.add(possible_coin)
-                        queue.append(possible_coin)
-            fewest_coin += 1
-        return -1
+        def dp(i,target):
+            
+            if (i,target) in cache:
+                return cache[(i,target)]
+            if target == 0:
+                return 0
+            if i == len(coins) or target < 0:
+                return amount + 1
+            res = dp(i + 1,target)
+            if target >= coins[i]:
+                res = min(res,dp(i,target - coins[i]) + 1)
+            cache[(i,target)] = res
+            return cache[(i,target)]
+        res = dp(0,amount)
+        return res if res != amount + 1 else -1
