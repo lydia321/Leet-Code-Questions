16d3ce090b9253148e58fe97f083e010333de107
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-02 11:30:53 +0300

    Time: 2954 ms (5.01%), Space: 466.2 MB (5.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        cache = {}
+        '''
+        Descions: 
+        1) we use the coin and increase our count and choose between all coins
+        2) we dont chose the coin, count is the same and choose between the rest of the coins
         
+        '''
+    
+        cache = {}
         def dp(i,target):
-            
+            nonlocal res
             if (i,target) in cache:
                 return cache[(i,target)]
             if target == 0:
                 return 0
-            if i == len(coins) or target < 0:
-                return amount + 1
-
-            cache[(i,target)] = min(dp(i + 1,target),dp(i,target - coins[i]) + 1)
+            if target < 0 or i == len(coins): #invalid case
+                return inf
+            cache[(i,target)] = min(dp(i+1,target), dp(i,target - coins[i]) + 1)
             return cache[(i,target)]
+        
         res = dp(0,amount)
-        return res if res != amount + 1 else -1
+        return res if res != inf else -1
+            
+            
+            
