723d417de80bc9e004c81288d327ba241bbb16cb
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-03 11:53:57 +0300

    Time: 66 ms (10.27%), Space: 16.7 MB (5.13%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,7 +1,8 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         cache = {}
-        def closest(i,num,days):
+        
+        def closest(i,num):
             key = days[i]
             while i < len(days) and key + num > days[i]:
                 i+=1
@@ -16,9 +17,9 @@ class Solution:
             #First choice : add the first cost and go to next index
             choice1 = dp(i+1) + costs[0]
             #second choice: add second cost and go to index larger index from days[i] + 7
-            choice2 = dp(closest(i,7,days)) + costs[1]
+            choice2 = dp(closest(i,7)) + costs[1]
             #third choice: add third cost and go to index larger index from days[i] + 30
-            choice3 = dp(closest(i,30,days)) + costs[2]
+            choice3 = dp(closest(i,30)) + costs[2]
             
             cache[i] = min(choice1,choice2,choice3)   
             return cache[i]
