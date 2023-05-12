6a0910068552a54dc6609ee397b23362bc79d31f
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-12 16:40:10 +0300

    Time: 58 ms (11.98%), Space: 16.3 MB (20.24%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -6,10 +6,53 @@ class Solution:
         for i in range(len(s)):
             lookup[s[i]] = i
        
-        
         for i in range(len(s)):
             if s[i] not in stack:
                 while stack and stack[-1] > s[i] and lookup[stack[-1]] > i:
                     stack.pop()
                 stack.append(s[i])
         return "".join(stack)
+    
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         lookup = {}
+#         stack = []
+        
+#         for i in range(len(s)):
+#             lookup[s[i]] = i
+       
+        
+#         for i in range(len(s)):
+#             if s[i] not in stack:
+#                 while stack and stack[-1] > s[i] and lookup[stack[-1]] > i:
+#                     stack.pop()
+#                 stack.append(s[i])
+#         return "".join(stack)
