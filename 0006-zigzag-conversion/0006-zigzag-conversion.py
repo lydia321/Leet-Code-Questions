c82b167a066e4f58a5f2eaa879813482940597f4
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-15 11:02:32 +0300

    Time: 83 ms (25.51%), Space: 16.6 MB (13.85%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,23 +1,26 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        matrix = [[] for i in range(len(s))]
+        matrix = []
+        for _ in range(numRows):
+            matrix.append([])
+        diagonal = False
         i = 0
-        
-        while i < len(s):   
-            #go vertical
-            for row in range(numRows):
-                if i < len(s):
-                    matrix[row].append(s[i])
-                    i += 1  
-            #go diagonal - up
-            for row in range(numRows - 2,0,-1):
-                if i < len(s):
-                    matrix[row].append(s[i])
-                    i += 1
-                
-
+        while i < len(s):
+            if not diagonal:
+                for row in matrix:
+                    if i < len(s):
+                        row.append(s[i])
+                        i += 1
+                diagonal = True
+            if diagonal:
+                for row in range(numRows - 2, 0, -1):
+                    if i < len(s):
+                        matrix[row].append(s[i])
+                        i += 1
+                diagonal = False
+        # print(matrix)
         res = ''
-        for row in matrix:
-            for char in row:
+        for rows in matrix:
+            for char in rows:
                 res += char
         return res
