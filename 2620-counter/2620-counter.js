17deb55ca6551929496c8c7ae9c6dfca2ce9866f
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-17 10:35:29 +0300

    Time: 64 ms (17.59%), Space: 41.7 MB (85.14%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -2,14 +2,9 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    return function() { 
-        return n++;
-        
-        
-    };
-};
-
+const createCounter = function(n){
+    return () => n++;
+}
 /** 
  * const counter = createCounter(10)
  * counter() // 10
