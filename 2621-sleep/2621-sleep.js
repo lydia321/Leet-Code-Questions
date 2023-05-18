e2cc61ffa354c9e2bc3ef379b593ec704206dc89
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-18 15:42:29 +0300

    Time: 56 ms (75.88%), Space: 41.4 MB (90.23%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -1,12 +1,14 @@
 /**
  * @param {number} millis
  */
-async function sleep(millis) {
-    return new Promise(function (resolve){
-        setTimeout(() => resolve(),millis)
-    });
+function sleep(millis){
+    function callback(resolve,reject) {
+        setTimeout(() => {
+            resolve();
+        },millis)
+    }
+    return new Promise(callback)
 }
-
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
