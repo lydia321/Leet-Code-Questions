aa7da39bfbb43f8d623797292059712457441ee1
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-19 22:26:33 +0300

    Time: 51 ms (84.93%), Space: 41.6 MB (61.06%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,11 +1,11 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
-    return function(...args) {
+const createHelloWorld = function(){
+    return function(...args){
         return "Hello World";
     }
-};
+}
 
 /**
  * const f = createHelloWorld();
