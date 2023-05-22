83a123e63b6e83a2377f7cb8937e12b809934015
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-22 12:16:16 +0300

    Time: 59 ms (68.94%), Space: 41.6 MB (84.27%) - LeetHub

--- 2627-debounce/2627-debounce.js
+++ 2627-debounce/2627-debounce.js
@@ -3,10 +3,18 @@
  * @param {number} t milliseconds
  * @return {Function}
  */
+/**
+Question breakdown: basically when a fn is excecuting after some t delay, if same fn is called before the previous one has excecuted, clear previous request and continue with the new request.
+If Fn has already executed, just continue with next request.
+
+Concept: - You set the time out to be delayed by the given t, to perform fn(...args).
+         - clearTimeout function will clear fn's that have not been excecuted when another fn is called
+         - If an Fn is excecuted before another fn is called, clearTimout wont affect anything
+*/
 var debounce = function(fn, t) {
     let id;
     return function(...args) {
-        clearTimeout(id);
+        clearTimeout(id); 
         id = setTimeout(() => {
             fn(...args)
         },t);
