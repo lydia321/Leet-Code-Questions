c0954518a21e734167dec264b0dc40f742b59919
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-23 11:21:15 +0300

    Time: 333 ms (47.27%), Space: 27.7 MB (5.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return
-        visited = set()
-        islands = 0
+        seen = set() 
+        row_bound = len(grid) 
+        col_bound = len(grid[0])
         
-        def dfs(x,y):
-            visited.add((x,y))
-            
-            for x,y in [x,y+1], [x,y-1], [x+1,y], [x-1,y]:
-                if x in range(len(grid)) and y in range(len(grid[0])) and (x,y) not in visited  and grid[x][y] == '1':
-                    dfs(x,y)
+        def dfs(row,col):
+            seen.add((row,col))
+            if (row + 1,col) not in seen and ((row + 1 ) < row_bound) and grid[row + 1][col] =='1':
+                dfs(row + 1,col)
+            if (row - 1,col) not in seen and ((row - 1 ) >= 0) and grid[row - 1][col] =='1':
+                dfs(row - 1,col)
+            if (row,col- 1) not in seen and ((col - 1 ) >= 0) and grid[row][col - 1] =='1':
+                dfs(row,col - 1)
+            if (row,col + 1) not in seen and ((col + 1 ) < col_bound) and grid[row][col + 1] == '1':
+                dfs(row,col + 1)
+        res = 0        
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if (row,col) not in seen and grid[row][col] == '1':
+                    res += 1
+                    dfs(row,col)
+                    # print(res,seen)
+       
+        return res
         
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == '1' and (i,j) not in visited:
-                    dfs(i,j)
-                    islands += 1
-        return islands
-    
+       
+      
