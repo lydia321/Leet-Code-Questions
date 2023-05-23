db359af404f5b99a89a27556a43f869885cf557f
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-23 15:01:28 +0300

    Time: 66 ms (36.11%), Space: 41.9 MB (85.27%) - LeetHub

--- 2676-throttle/2676-throttle.js
+++ 2676-throttle/2676-throttle.js
@@ -3,27 +3,38 @@
  * @param {number} t
  * @return {Function}
  */
-var throttle = function(fn, t) {
-  let timeoutInProgress = null;
-  let argsToProcess = null;
-  
-  const timeoutFunction = () => {
-    if (argsToProcess === null) {
-      timeoutInProgress = null; // enter the waiting phase
-    } else {
-      fn(...argsToProcess);
-      argsToProcess = null;
-      timeoutInProgress = setTimeout(timeoutFunction, t);
-    }
-  };
+/**
+Eg. Leetcode RunCode
+Concept Prevent a code from being called more frequently than it can handle.
+Recursion concept: recursivly call nextargs when its time, if any
 
-  return function throttled(...args) {
-    if (timeoutInProgress) {
-      argsToProcess = args;
-    } else {
-      fn(...args); // enter the looping phase
-      timeoutInProgress = setTimeout(timeoutFunction, t);
-    }
+1) if its not ready to excecute it should override any nextargs if any
+2)else excecute and set timer for when your ready to exceute again. if you have waiting args 
+
+*/
+var throttle = function(fn, t) {
+    let readytoexecute = null;
+    let nextargs = null;
+    
+    const nextfn = () => { // will excecute next args, if any
+        if (nextargs == null){
+            readytoexecute = null;
+        }else{
+            fn(...nextargs);
+            nextargs = null;
+            readytoexecute = setTimeout(nextfn,t);
+        }       
+    };
+    
+  return function(...args) {
+      if (readytoexecute){
+          nextargs = args; //override any waiting next args
+          
+      }else{
+          fn(...args);
+          // its gonna start executing after certain  amount of time, if it has next args
+          readytoexecute = setTimeout(nextfn,t); 
+      }
   }
 };
 
