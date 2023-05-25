7c0da8e99637cf0b648ab43067d34caf13510ed2
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-05-25 16:27:21 +0300

    Time: 112 ms (11.71%), Space: 75.7 MB (26.85%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,5 +3,27 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-    return JSON.stringify(object)
+    //Base Case, you can directly convert to string
+    if (object === null || object === undefined){
+        return String(object);
+    }
+    //Array
+    if (Array.isArray(object)){
+        const res = object.map((obj) => jsonStringify(obj));
+        return `[${res.join(',')}]`;
+    }
+    //Object
+    if (typeof object === 'object'){
+        const keys = Object.keys(object);//get all keys to a constant variable obj
+        const res = keys.map((key) => `"${key}":${jsonStringify(object[key])}`);
+        return `{${res.join(',')}}`
+                 
+    }
+    if (typeof object === 'string'){
+        return `"${String(object)}"`
+    }
+    
+    
+    
+    return String(object); // for booleans and numbers;
 };
