e3ff35e158e203910ca0ad49ae1a652345b339f2
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-06-07 21:01:51 +0300

    Time: 45 ms (53.19%), Space: 16.3 MB (25.27%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -4,9 +4,9 @@ class Solution:
         print(n)
         flip = 0
         for i in range(n):
-            mask = 1<<i
+            mask = 1<<i #position its at
             if c&mask:      #at a position, if c has a 1, then either b or a should have a 1, else flip one bit.
-                if not (a&mask | b&mask):
+                if not (a&mask | b&mask):#if both are not 1
                     flip+=1
             else:           #if c has 0, then both need to be 0. If not, then flip those bits.
                 if a&mask:
