7adec20d863c1c9ce14c973a015234e11adf29f5
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-07-14 11:11:58 +0300

    Time: 66 ms (91.03%), Space: 18.4 MB (21.60%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         dict = Counter(nums)
-        # print(dict)
-        
+      
         for i,count in dict.items():
             if count == 1:
                 return i
