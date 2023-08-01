9000d2524ae30a6979e66f94c9b0d0d52bd8e594
lydia321 <42097132+lydia321@users.noreply.github.com>
2023-08-01 10:58:07 +0300

    Time: 587 ms (74.87%), Space: 16.2 MB (93.72%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,11 +1,29 @@
 class Solution:
-    def countQuadruplets(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    val = nums[i] + nums[j] + nums[k]
-                    for r in range(k + 1,len(nums)):
-                        if val == nums[r]:
-                            res += 1
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        res = []
+        pac, atl = set(), set()
+        def dfs(r,c,prev,visited):
+            if (r,c) in visited or r < 0 or c < 0 or r == rows or c == cols or heights[r][c] < prev:
+                return 
+            visited.add((r,c))
+            dfs(r + 1,c,heights[r][c],visited)
+            dfs(r - 1, c,heights[r][c],visited)
+            dfs(r, c + 1, heights[r][c],visited)
+            dfs(r,c - 1, heights[r][c],visited)
+ 
+        for c in range(cols):
+            dfs(0,c, heights[0][c],pac)
+            dfs(rows - 1,c,heights[rows - 1][c],atl)
+        for r in range(rows):
+            dfs(r,0,heights[r][0],pac)
+            dfs(r,cols - 1,heights[r][cols - 1],atl)
+        #Atlantic 
+       
+        res = []
+        for p in pac:
+            if p in atl:
+                res.append(p)
         return res
+        
+        
