d5b17ed122187c2af77df3bbb1c03a936cd57d7a
lydia321 <42097132+lydia321@users.noreply.github.com>
2024-02-05 22:59:23 +0300

    Time: 145 ms (79.55%), Space: 17.8 MB (71.24%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -1,18 +1,13 @@
-class Solution(object):
-    def canBeValid(self, s, locked):
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        
         if len(s) % 2:  # Intuitively, odd-length s cannot be valid.
             return False
-
-        # traverse each parenthesis forward, treat all unlocked Parentheses as'(' and check if there is ')'
-        # that cannot be eliminated by previous '(', if it exists, then the input s can't be valid.
-        balance = 0
+        valid = 0
         for i in range(len(s)):
-            balance += 1 if s[i] == '(' or locked[i] == '0' else -1
-            if balance < 0:
+            valid += 1 if s[i] == '(' or locked[i] == '0' else -1   
+            if valid < 0:
                 return False
-
-        # traverse each parenthesis backward, treat all unlocked Parentheses as')' and check if there is '('
-        # that cannot be eliminated by previous ')', if it exists, then the input s can't be valid.
         balance = 0
         for i in range(len(s) - 1, -1, -1):
             balance += 1 if s[i] == ')' or locked[i] == '0' else -1
