f26318f241a9523a78ea184c5ee3b1cc7bbbda19
lydia321 <42097132+lydia321@users.noreply.github.com>
2024-02-05 23:04:49 +0300

    Time: 163 ms (61.57%), Space: 17.8 MB (82.25%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -17,6 +17,12 @@ class Solution:
 
         return True
     
-    
+#         balance = 0
+#         for i in range(len(s) - 1, -1, -1):
+#             balance += 1 if s[i] == ')' or locked[i] == '0' else -1
+#             if balance < 0:
+#                 return False
+
+#         return True
         
         
